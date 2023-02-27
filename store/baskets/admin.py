48fe6199c75a55c2eed1131af5829d43496f5604
Koskay <68971733+Koskay@users.noreply.github.com>
2023-02-27 22:07:27 +0300

    Update admin.py

--- store/baskets/admin.py
+++ store/baskets/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Basket, BasketProductQuantity
+from .models import Basket
 
 
 admin.site.register(Basket)
-admin.site.register(BasketProductQuantity)
