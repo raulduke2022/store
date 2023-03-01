625d55b16864d668d8050a15276fe5bc70948faa
raulduke2022 <sergeytersahakyan@gmail.com>
2023-03-01 09:44:15 +0300

    some changes

--- store/baskets/views.py
+++ store/baskets/views.py
@@ -7,8 +7,9 @@ def add_product(request, product_id):
     basket = Basket.objects.filter(user=request.user)
     if basket:
         if basket[0].products.filter(id=product_id):
-            basket[0].cartproduct_set.filter(product__id=product_id)[0].quantity = 10
-            basket[0].cartproduct_set.filter(product__id=product_id)[0].save()
+            product_cart = basket[0].cartproduct_set.filter(product__id=product_id)[0]
+            product_cart.quantity += 1
+            product_cart.save()
         else:
             basket[0].products.add(product_id)
             CartProduct.objects.create(product=Product.objects.get(pk=product_id), basket=basket[0], quantity=1)
